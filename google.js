1aee7b4abe36b0bf18be0fa87a8a36b30dd6fcad
jnd0 <besteam@best.com>
2018-03-07 21:34:53 +0000

    google.html - example working with a json file google.js - javascript code to make it work randomData.json - the "fake data" I've used for the example

---
+++ google.js
@@ -0,0 +1,69 @@
+// Load the Visualization API and the piechart package.
+google.charts.load('current', {'packages': ['treemap']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    $.getJSON("randomData.json", function () {
+    })
+        .done(function (json) {
+
+
+            const data = new google.visualization.DataTable();
+
+
+            data.addColumn('string', 'ID');
+            data.addColumn('string', 'Parent');
+            data.addColumn('number', 'Number Of Lines');
+
+            data.addRow(['Entities', null, 0]);
+
+            /*   data.addRow(["caca", "Entities", null]);
+               data.addRow(["da", "Entities", null]);
+               data.addRow(["oa", "Entities", null]);
+
+               data.addRow(["dsds", "caca", 55]);
+               data.addRow(["owa", "da", null]);
+   */
+
+            for (let [key, value] of Object.entries(json)) {
+
+                data.addRow([key, "Entities", null]);
+
+                for(let [k2, v2] of Object.entries(value)){
+                    data.addRow([k2, key, parseInt(v2)]);
+                }
+            }
+
+            const options = {
+                highlightOnMouseOver: true,
+                maxDepth: 1,
+                maxPostDepth: 2,
+                minHighlightColor: '#8c6bb1',
+                midHighlightColor: '#9ebcda',
+                maxHighlightColor: '#edf8fb',
+                minColor: '#009688',
+                midColor: '#f7f7f7',
+                maxColor: '#ee8100',
+                headerHeight: 15,
+                showScale: true,
+                height: 500,
+                useWeightedAverageForAggregation: true
+            };
+
+            console.log(document.getElementById('chart_div'));
+
+            const chart = new google.visualization.TreeMap(document.getElementById('chart_div'));
+
+            chart.draw(data, options);
+
+        })
+        .fail(function () {
+            console.log("error");
+        })
+        .always(function () {
+            console.log("complete");
+        });
+
+}
